7f99e773895cc3e83b2f54170eacd322573ef175
sushant09112004 <jadhavsushant379@gmail.com>
2026-01-02 18:16:18 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,12 @@
 import Image from "next/image";
-
+import Landing from "@/components/Landing/Landing";
 export default function Home() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-white text-black px-4">
-      <h1 className="text-2xl md:text-4xl font-bold text-center">
+      {/* <h1 className="text-2xl md:text-4xl font-bold text-center">
         Welcome to My personalized email automation application
-      </h1>
+      </h1> */}
+      <Landing/>
     </div>
   );
 }
